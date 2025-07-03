c45250770187547fa8a9252597a7b4ed01fd84c3
JuniorSarh <sabelohgumede@gmail.com>
2025-07-03 14:43:28 +0200

    updates

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-//JavaScript for button request
-function RequestBtn() {
+//JavaScript for button request 
+
+
+function RequestBtn() 
+{
       alert('Your request has been submited successfully!!');
-      console.log("testing 11");
+      
+       const person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+document.getElementById("myForm").innerHTML = person.fullName();
+
+      
     }
+    console.log("testing 11");
+
+
+
+    
+    
+    
 
